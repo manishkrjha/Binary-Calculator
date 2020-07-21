ef2ba84323e08057ecabd68f80658a7630675777
Manish Jha <codernibba@gmail.com>
2020-07-21 19:10:57 +0530

    updated javascript

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -5,8 +5,10 @@ let into = document.getElementById('btnMul');
 let divide = document.getElementById('btnDiv');
 let resultBox = document.getElementById('res');
 
-var val1 = [];
-var val2 = [];
+// var val1 = [];
+// var val2 = [];
+var val1;
+var val2;
 var clicked = false;
 
 plus.addEventListener("click", function(){
@@ -36,55 +38,54 @@ divide.addEventListener("click", function(){
 function pushZero(){
     if(clicked){
         //if middleOperator is pressed
-        val2.push(0);
+        // val2.push(0);
         resultBox.innerHTML += '0';
+        val2 = resultBox.innerText;
     }else{
         //if middleOperator is not pressed
-        val1.push(0);
+        // val1.push(0);
         resultBox.innerHTML += '0';
+        val1 = resultBox.innerText;
     }    
 }
 
 function pushOne(){
     if(clicked){
         //if middleOperator is pressed
-        val2.push(1);
+        // val2.push(1);
         resultBox.innerHTML += '1';
+        val2 = resultBox.innerText;
     }else{
         //if middleOperator is not pressed
-        val1.push(1);
+        // val1.push(1);
         resultBox.innerHTML += '1';
+        val1 = resultBox.innerText;
     }    
 }
 
 //passing array to this function
 function binaryToNum(a){
     let num = 0;
-
     for(var i=0; i<a.length; i++){
-        num += a[i]*(Math.pow(2, (a.length-1-i)));
+       num +=  a[i]*Math.pow(2, a.length-i-1);
     }
 
     return num;
 }
 
 //Function to convert a number to binary
-function numToBinary(x){
-    var ar = [];
-    if(x==1){
-        ar.push(1);
-    }else{
-        ar.push(x%2);
-        x=x/2;
-        return numToBinary(x);
-    }
-
-    ar.reverse();
-
-    for(var a=0; a<ar.length; a++){
-        console.log(ar[a]);
-    }
-}
+// function numToBinary(x){
+//     var s='';
+//     let n;
+//     while(x>=1){
+//         n = x%2;
+//         s += `$`;
+//         x /= 2;
+//     }
+ 
+//     // s = 'ans';
+//     return s;
+// }
 
 function result(middleOperator){
     var ans;
@@ -107,7 +108,9 @@ function result(middleOperator){
         ans = num1/num2;
     }
 
-    return numToBinary(ans);
+    var output = ans;
+    
+    resultBox.innerHTML = output;
 }
 
 // function clear(){
